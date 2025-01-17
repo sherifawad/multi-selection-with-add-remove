2f0629e423bba22cb692ae649ebce470f931cdc7
Sherif Awad <masksherif@outlook.com>
2025-01-17 19:36:34 +0200

    fix: url

--- components/installation.tsx
+++ components/installation.tsx
@@ -9,10 +9,10 @@ export default function Installation() {
 	return (
 		<div className="max-w-md px-8">
 			<CodeBlockCommand
-				__bunCommand__={`bunx --bun shadcn@latest add ${baseUrl}multi-select`}
-				__npmCommand__={`npx shadcn@latest add ${baseUrl}multi-select`}
-				__yarnCommand__={`npx shadcn@latest add ${baseUrl}multi-select`}
-				__pnpmCommand__={`pnpm dlx shadcn@latest add ${baseUrl}multi-select`}
+				__bunCommand__={`bunx --bun shadcn@latest add ${baseUrl}multi-selection`}
+				__npmCommand__={`npx shadcn@latest add ${baseUrl}multi-selection`}
+				__yarnCommand__={`npx shadcn@latest add ${baseUrl}multi-selection`}
+				__pnpmCommand__={`pnpm dlx shadcn@latest add ${baseUrl}multi-selection`}
 			/>
 		</div>
 	);
